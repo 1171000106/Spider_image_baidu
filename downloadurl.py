d9b9579149f54f53e9d19765d34da086fce4f3b2
dxyinme <dxyinme@outlook.com>
2019-04-06 00:20:38 +0800

    修改downloadurl.py

--- downloadurl.py
+++ downloadurl.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 import requests
+import os
 
 if(__name__ == "__main__"):
     F = open("Need.txt");
@@ -12,11 +13,13 @@ if(__name__ == "__main__"):
         cd.append(L);
     F.close();
     l = len(cd);
+    if(os.path.exists(Gs) == False):
+        os.mkdir(Gs);
     for i in range(0,l):
-        qa = cd[i].replace("\n","").split(".");
-        cc = qa[len(qa)-1];
-        url = requests.get(cd[i].replace("\n",""));
-        print(str(i)+"."+cc);
-        with open(str(i)+cc,"wb") as f:
+        qa = cd[i].replace("\n","").split("/");
+        cc = qa[len(qa)-1].replace("?","").replace("*","");
+        url = requests.get(cd[i].replace("\n",""),allow_redirects=False);
+        print(cc);
+        with open(Gs+"/"+cc,"wb") as f:
             f.write(url.content);
                 
